81a6925995626adc3044cc34005323bc36a19878
alpernuage <alperakbulut@gmail.com>
2025-09-05 15:41:54 +0200

    update access_control rules

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -14,8 +14,6 @@ class HomeController extends AbstractController
     #[Route('/', name: 'app_home')]
     public function index(QuizService $quizService): Response
     {
-        $this->denyAccessUnlessGranted('ROLE_USER');
-
         return $this->render('home/index.html.twig', [
             'quizzes' => $quizService->findLastGeneratedQuizzes(),
         ]);
